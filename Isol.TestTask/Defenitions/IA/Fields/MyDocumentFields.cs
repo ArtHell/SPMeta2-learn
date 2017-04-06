7eae6828099e84c09f83097b0d92caa7b4cba216
a.omelchuk <a.omelchuk@itransition.com>
2017-04-06 20:01:20 +0300

    fixed internal names

--- Isol.TestTask/Defenitions/IA/Fields/MyDocumentFields.cs
+++ Isol.TestTask/Defenitions/IA/Fields/MyDocumentFields.cs
@@ -14,7 +14,7 @@ namespace Isol.TestTask.Defenitions.IA.Fields
         public static TaxonomyFieldDefinition DocumentType = new TaxonomyFieldDefinition()
         {
             Title = Const.FieldNames.ProjectDocumentFields.DocumentType,
-            InternalName = Const.InternalPrefixes.ProjectDocument + Const.FieldNames.ProjectDocumentFields.DocumentType,
+            InternalName = Const.InternalPrefixes.ProjectDocument + Const.FieldNames.ProjectDocumentFields.DocumentType.Trim(),
             Group = Const.Groups.DefaultFieldsGroup,
             Id = new Guid("ba562d6b-7e59-492b-8677-aadf31887033"),
             IsMulti = false,
@@ -28,7 +28,7 @@ namespace Isol.TestTask.Defenitions.IA.Fields
         public static FieldDefinition ExpirationDate = new FieldDefinition
         {
             Title = Const.FieldNames.ProjectDocumentFields.ExpirationDate,
-            InternalName = Const.InternalPrefixes.ProjectDocument + Const.FieldNames.ProjectDocumentFields.ExpirationDate,
+            InternalName = Const.InternalPrefixes.ProjectDocument + Const.FieldNames.ProjectDocumentFields.ExpirationDate.Trim(),
             Group = Const.Groups.DefaultFieldsGroup,
             FieldType = BuiltInFieldTypes.DateTime,
             Id = new Guid("47ff2a80-de24-4289-b80f-52ec3d826cb3")
@@ -37,7 +37,7 @@ namespace Isol.TestTask.Defenitions.IA.Fields
         public static FieldDefinition BelongToProject = new LookupFieldDefinition()
         {
             Title = Const.FieldNames.ProjectDocumentFields.BelongToProject,
-            InternalName = Const.InternalPrefixes.ProjectDocument + Const.FieldNames.ProjectDocumentFields.BelongToProject,
+            InternalName = Const.InternalPrefixes.ProjectDocument + Const.FieldNames.ProjectDocumentFields.BelongToProject.Trim(),
             Group = Const.Groups.DefaultFieldsGroup,
             FieldType = BuiltInFieldTypes.Lookup,
             Id = new Guid("3f821c2b-e60c-40b8-8bee-c39bed085641"),
@@ -47,7 +47,7 @@ namespace Isol.TestTask.Defenitions.IA.Fields
         public static FieldDefinition DocumentResponsible = new FieldDefinition
         {
             Title = Const.FieldNames.ProjectDocumentFields.DocumentResponsible,
-            InternalName = Const.InternalPrefixes.ProjectDocument + Const.FieldNames.ProjectDocumentFields.DocumentResponsible,
+            InternalName = Const.InternalPrefixes.ProjectDocument + Const.FieldNames.ProjectDocumentFields.DocumentResponsible.Trim(),
             Group = Const.Groups.DefaultFieldsGroup,
             FieldType = BuiltInFieldTypes.User,
             Id = new Guid("db4b1422-762a-4be7-bcbe-40e657a733f1")
